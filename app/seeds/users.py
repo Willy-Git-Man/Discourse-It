28b4da2490366977c3beb3f842694f392426ce25
Willy-Git-Man <83353737+Willy-Git-Man@users.noreply.github.com>
2022-03-16 12:37:20 -0400

    post route tested and working

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -21,7 +21,7 @@ def seed_users():
 # SQLAlchemy doesn't have a built in function to do this
 # TRUNCATE Removes all the data from the table, and RESET IDENTITY
 # resets the auto incrementing primary key, CASCADE deletes any
-# dependent entities
+# dependent entitiesf
 def undo_users():
     db.session.execute('TRUNCATE users RESTART IDENTITY CASCADE;')
     db.session.commit()
