5c3540b474caf4ef7cd7840d1c9b17477c86ca34
Willy-Git-Man <83353737+Willy-Git-Man@users.noreply.github.com>
2022-03-16 14:18:03 -0400

    dispatch for all channels hitting on user page so loading on first render of the user page

--- react-app/src/components/UsersList.js
+++ react-app/src/components/UsersList.js
@@ -1,10 +1,14 @@
 import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
 import { NavLink } from 'react-router-dom';
+import { getAllChannelsThunk } from '../store/channels';
 
 function UsersList() {
+  const dispatch = useDispatch()
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
+    dispatch(getAllChannelsThunk())
     async function fetchData() {
       const response = await fetch('/api/users/');
       const responseData = await response.json();
