f2e75e756229ba15ecde166d97666aa5fbebf5f1
Willy-Git-Man <83353737+Willy-Git-Man@users.noreply.github.com>
2022-03-18 12:39:21 -0400

    everything working friday at 1240 first feature done second feature get working but not getting the right posts per channel yet

--- react-app/src/components/Posts/channelPosts.js
+++ react-app/src/components/Posts/channelPosts.js
@@ -7,39 +7,55 @@ import "./index.css";
 export default function ChannelPosts() {
   const dispatch = useDispatch();
   const sessionUser = useSelector((state) => state.session.user);
+  const { channelId, userId } = useParams();
   const channelArray = useSelector((state) =>
-  Object.values(state.channels.channels)
-);
+    Object.values(state.channels.channels)
+  );
 
-console.log('channelArray:', channelArray)
+  console.log("channelArray:", channelArray);
 
   const postArray = useSelector((state) => Object.values(state.posts.posts));
+  const hello = postArray.filter((post) => post.channel_id === channelId)
+  console.log('hello:', hello)
   console.log("postState:", postArray);
-  const { channelId, userId } = useParams();
   const ID = channelId;
   // console.log("ID:", ID);
   // console.log("channelId:", channelId, "userId:", userId);
   useEffect(() => {
-    dispatch(getAllPostsThunk(ID));
+    dispatch(getAllPostsThunk());
   }, [dispatch, ID]);
 
-  console.log("Channel Name:", )
+  console.log("Channel Name:");
 
   return (
     <div className="channelPostsMainDiv">
+
       <h1>Hello Will</h1>
+      <NavLink
+            to={`/home`}
+          >
+           home
+          </NavLink>
 
       {postArray.map((post) => (
-        <div key={post.id} className="eachChannelPostDiv">
+        // <div>
+        // {post.channel_id === channelId &&
+          <div key={post.id} className="eachChannelPostDiv">
+            {post.channel_id === +channelId &&
+<h1>Hello</h1>
+        }
           <img
-            className="postPicture"
-            src={post.channel_picture}
-            alt="Broken Img URL"
+          className="postPicture"
+          src={post.channel_picture}
+          alt="Broken Img URL"
           />
           Link: to this channel page:{" "}
           <p className="eachPostTitle">
-            Id {post.id} -- Channel Id: {post.channel_id} -- Title: {post.title}
+          Id {post.id} -- Channel Id: {post.channel_id} -- Title:{" "}
+          {post.post_title}
           </p>
+          {/* </div>
+        } */}
         </div>
       ))}
     </div>
