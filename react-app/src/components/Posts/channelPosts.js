ad888cc25a983e519b5860c2c44257d0cc686bb4
Willy-Git-Man <83353737+Willy-Git-Man@users.noreply.github.com>
2022-03-18 13:52:37 -0400

    everything is working the posts are living on the right channel now

--- react-app/src/components/Posts/channelPosts.js
+++ react-app/src/components/Posts/channelPosts.js
@@ -42,6 +42,20 @@ export default function ChannelPosts() {
   console.log("channelKeysArray at channel Id:", channelKeysArray[+channelId]);
 
   const postArray = useSelector((state) => Object.values(state.posts.posts));
+  const postKeysArray = useSelector((state) => Object.keys(state.posts.posts));
+
+
+
+  const object2 = {}
+  const test2 = postKeysArray.forEach((key, i) => {
+    object2[key] = postArray[i];
+  });
+
+  console.log("Test Two:", object2)
+
+
+
+  console.log('postKeysArray:', postKeysArray)
   console.log("postArray:", postArray);
   // const ID = channelId;
 
@@ -51,11 +65,28 @@ export default function ChannelPosts() {
     <div className="channelPostsMainDiv">
       <h1>{object[channelId].channel_name}</h1>
       <NavLink to={`/home`}>home</NavLink>
+      {postKeysArray?.filter((key) => postArray[key -1]?.channel_id === +channelId).map((post) => (
+        <div key={post.id} className="eachChannelPostDiv">
+        {post.channel_id === +channelId && (
+          <div>
+            <h1>{post.post_title}</h1>
+          </div>
+        )}
+        <img
+          className="postPicture"
+          src={post.channel_picture}
+          alt="Broken Img URL"
+        />
+        Link: to this channel page:{" "}
+        <p className="eachPostTitle">
+          Id {post.id} -- Channel Id: {post.channel_id} -- Title:{" "}
+          {post.post_title}
+        </p>
+      </div>
+      ))}
 
       {/* {postArray.map((post) => ( */}
-      {postArray.map((post) => (
-        // {post.channel_id === +channelId && (
-        // {post.channel_id === channelId &&
+      {/* {postArray.map((post) => (
 
         <div key={post.id} className="eachChannelPostDiv">
           {post.channel_id === +channelId && (
@@ -73,10 +104,8 @@ export default function ChannelPosts() {
             Id {post.id} -- Channel Id: {post.channel_id} -- Title:{" "}
             {post.post_title}
           </p>
-          {/* </div>
-        } */}
         </div>
-      ))}
+      ))} */}
     </div>
   );
 }
