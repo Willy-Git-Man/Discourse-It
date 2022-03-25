a5ed3c45efc1768533a39e745836f5b8ee6274d3
Willy-Git-Man <83353737+Willy-Git-Man@users.noreply.github.com>
2022-03-25 11:16:26 -0400

    style changes friday morning

--- react-app/src/components/Footer/footer.js
+++ react-app/src/components/Footer/footer.js
@@ -5,7 +5,7 @@ import './index.css'
 const Footer = () => {
   return (
     <footer className="loginFooter">
-      <a className="footerNavLinks" href='https://github.com/Willy-Git-Man/Cartoonstagram' target='_blank' rel='noopener noreferrer'>GitHub</a>
+      <a className="footerNavLinks" href='https://github.com/Willy-Git-Man' target='_blank' rel='noopener noreferrer'>GitHub</a>
       <div />
       <a className="footerNavLinks" href='https://www.linkedin.com/in/william-b-grossman/' target='_blank' rel='noopener noreferrer'>LinkedIn</a>
     </footer>
